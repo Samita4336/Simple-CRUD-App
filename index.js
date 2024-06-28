6d7525af14d3f13668a1d43b68f76b86980a7df8
Samuel <samuel@eaglelionsystems.com>
2024-06-28 04:43:23 -0400

    delete  product api

--- index.js
+++ index.js
@@ -41,11 +41,27 @@ app.put('/api/product/:id', async (req, res) => {
         const {id} = req.params
         const product = await Product.findByIdAndUpdate(id, req.body)
         if (!product) {
-            res.status(404).json({message: 'Product not found!'})
+          return  res.status(404).json({message: 'Product not found!'})
         }
         const updateProduct = await Product.findById(id)
 
         res.status(200).json(updateProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//delete a product
+
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndDelete(id)
+        if (!product) {
+            return res.status(404).json({message: "Product not found!"})
+        }
+        res.status(200).json({message: "Product deleted successfully"})
         
     } catch (error) {
         res.status(500).json({message: error.message})
